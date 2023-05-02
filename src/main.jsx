8f5d62f9c0e8d60334021887727ab2f650935170
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-05-02 11:38:37 -0600

    change font with react

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SelectorDeClases from './SelectorDeClases'
+import './SelectorDeClases.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SelectorDeClases />
+  </React.StrictMode>,
+)
